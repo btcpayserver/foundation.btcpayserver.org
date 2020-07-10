cc7a6177f36c8a852bddee70d908aced16558786
Andrew Camilleri <evilkukka@gmail.com>
2020-07-10 14:30:00 +0200

    redo

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+﻿window.onload = function () {
+    var btn = document.getElementById("navbar-toggler-btn");
+    var target = btn.getAttribute("data-target").substr(1);
+    var elem = document.getElementById(target);
+    btn.addEventListener("click", function () {
+        if (elem.classList.contains("show")) {
+            elem.classList.remove("show");
+        } else {
+            elem.classList.add("show");
+        }
+    });
+};
